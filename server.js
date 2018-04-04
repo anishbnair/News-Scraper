41865ad05149736ccb0e3e0c9a90080569c21520
anishbnair <nbanish@gmail.com>
2018-04-04 09:50:58 -0400

    included dependencies db and port details

---
+++ server.js
@@ -0,0 +1,54 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+// Port
+var PORT = process.env.PORT || 8080;
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Serve static content
+app.use(express.static("public"));
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newsScraper", {
+    useMongoClient: true
+});
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
+});
+
+// Listen on port
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+});
