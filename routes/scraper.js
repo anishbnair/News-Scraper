651425e9cbcf7692c077829e3fedb5e46e176fcc
anishbnair <nbanish@gmail.com>
2018-04-04 22:02:53 -0400

    included router and code to get saved articles from db

--- routes/scraper.js
+++ routes/scraper.js
@@ -10,3 +10,23 @@ var Article = require("../models/Article.js");
 router.get("/", function (req, res) {
     res.render("index");
 });
+
+// This will get the articles scraped and saved in db and show them in list
+router.get("/savedarticles", function (req, res) {
+
+    // Grab every doc in the Articles array
+    Article.find({}, function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+            var hbsArticleObject = {
+                articles: doc
+            };
+
+            res.render("savedarticles", hbsArticleObject);
+        }
+    });
+});
